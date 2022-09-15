df5f635d74a6e7b0613b6049350d5aaa31cadc7e
aqsafaisal34 <aqsay.faisal34@gmail.com>
2022-09-15 07:42:17 +0500

    work done

--- src/index.js
+++ src/index.js
@@ -3,20 +3,54 @@ import ReactDOM from 'react-dom';
 import { useState } from 'react';
 import "./style.css"
 
+
 function Room() {
   const [isLit, setLit] = useState(true);
   return (
+    <div className="task1">
+    <h1>The ON/OFF Programme</h1>
     <div className={`room ${isLit ? "lit" : "dark"}`}>
-   <div className="theme">Light is {isLit ? 'On' : 'Off'}
-   </div>
-    <button onClick={() => setLit(!isLit)}>
+  <h1 className="theme"> Light is {isLit ? 'On' : 'Off'}
+  </h1>
+    <button onClick={() => setLit (!isLit)}>
     ON
     </button>
-    <button onClick={() => setLit(!isLit)}>
+    <button onClick={() => setLit  (!isLit)}>
     OFF
     </button>
+    </div>
     </div>
   );
 }
 
-ReactDOM.render(<Room />, document.getElementById('root'));
+function Task() {
+  const [num,setNum]= useState(0);
+  const IncNum =()=>{
+    setNum(num +1);
+  };
+
+  const DeNum =()=>{
+    setNum( num-1);
+  }
+  return (
+    <div className='main'>
+
+    <h1>Increment/Decrement Programme</h1>
+    <div className="task">
+    <button onClick={IncNum}>Add(+)</button>
+
+
+    <h1>{num}</h1>
+    
+    <button onClick={DeNum}>Subtract(-)</button>
+    </div>
+    </div>
+
+  )
+ }
+
+ReactDOM.render(
+<>
+<Room />
+<Task/>
+</>, document.getElementById('root'));
